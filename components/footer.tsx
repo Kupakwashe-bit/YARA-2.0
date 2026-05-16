c54b2f98230e1eb01899d108af37c5d276e8a059
Kupakwashe-bit <nyangurukupakwashe@gmail.com>
2026-05-16 12:14:33 +0200

    Added tiktok social

--- components/footer.tsx
+++ components/footer.tsx
@@ -48,6 +48,15 @@ const socialLinks = [
       </svg>
     ),
   },
+  {
+    name: "TikTok",
+    href: "#",
+    icon: (
+      <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+        <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.77 0 2.89 2.89 0 0 1 2.89-2.89h.6V8.65h-.6a6.33 6.33 0 1 0 6.33 6.33V8.65c1.84.97 4.07.87 5.77-.35V6.69z" />
+      </svg>
+    ),
+  },
 ]
 
 const footerLinks = {
